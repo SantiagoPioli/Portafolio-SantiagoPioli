8a998ed04b68949448705dc72d1f5cfd2f9f8ede
Santiago Pioli <spioli16@hotmail.com>
2025-09-08 10:45:49 -0300

    Correcion de alerta

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
 // Enviar formulario (simulado)
-document.getElementById("contactForm").addEventListener("submit", function(event) {
-    event.preventDefault();
+document.getElementById("contactForm").addEventListener("submit", function() {
     alert("¡Gracias por contactarme! Te responderé pronto.");
-    this.reset();
 });
